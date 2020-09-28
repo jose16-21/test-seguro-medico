9f3d129aa2e470643fa6e70c1cee89ce6e1d4e06
Juan Jose Hernandez <jjhernandez@bdgsa.net>
2020-09-27 21:24:40 -0600

    Agregar archivos de proyecto.

---
+++ Contexts/AppDBContext.cs
@@ -0,0 +1,17 @@
+﻿// using Microsoft.EntityFrameworkCore;
+
+
+namespace SegundoParcial
+{
+    public class AppDBContext //: DbContext
+    {
+
+        //public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        //{
+
+        //}
+
+
+
+    }
+}
